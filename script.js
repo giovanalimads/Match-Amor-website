7a28df231fff9cdbc7162c6062d3f2774544d1b2
giovanalimads <gilimaolliveira@gmail.com>
2024-06-12 19:16:26 -0300

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// DOM
+const sr = ScrollReveal ({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.box-left', {delay: 100, origin: 'top'});
+sr.reveal('.img-right', {delay: 300, origin:'right'});
+sr.reveal('.title-section', {delay: 200, origin: 'top'});
+sr.reveal('.container-objectives .box .box-objective', {delay: 200, origin: 'top'});
+sr.reveal('.container-img-price .img-plans img', {delay: 200, origin: 'left'});
+sr.reveal('.container-plans', {delay: 200, origin: 'bottom'});
